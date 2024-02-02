31c246bb64517843f4aabe95b6a4472a5897bfec
yunchan312 <phenomenal312@hufs.ac.kr>
2024-02-03 01:05:23 +0900

    feat: about page

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,11 +1,11 @@
-export default function Card({ Icon, title, content }) {
+export default function Card({ Icon, title }) {
   return (
-    <div className="bg-white w-full py-3 px-10 h-24 flex flex-col justify-center rounded-xl shadow-2xl hover:cursor-pointer hover:scale-[1.1]">
-      <div className="flex gap-3">
-        <div className="text-4xl">{Icon}</div>
-        <div className="text-2xl">{title}</div>
+    <div className="bg-white w-full py-3 px-10 h-24 flex justify-between items-center hover:cursor-pointer hover:scale-[1.01]">
+      <div className="flex items-center gap-3">
+        <div className="text-[4vw]">{Icon}</div>
+        <div className="text-[4vw]">{title}</div>
       </div>
-      <div className="pl-2">{content ? content : "-"}</div>
+      <div className="text-[4vw]">{">>"}</div>
     </div>
   );
 }
--- src/Components/Info.js
+++ src/Components/Info.js
@@ -1,36 +1,25 @@
 import Card from "./Card";
 import { IoLogoGithub } from "react-icons/io5";
-import { FaInstagram, FaPhone } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
 import { MdOutlineEmail } from "react-icons/md";
 import { FaSchool } from "react-icons/fa";
-import { SiNotion, SiPhonepe } from "react-icons/si";
+import { SiNotion } from "react-icons/si";
+import TextSlider from "./TextSlider";
 
 export default function Info() {
   return (
-    <div className="bg-slate-400 py-10 min-h-[90vh] grid md:grid-cols-2 grid-cols-1">
-      <div className=" text-white font-bold md:text-[7vw] text-[10vw] px-10 mb-10">
-        ALL I NEED IS FINGER TO PRESS MOUSE
+    <div className="bg-slate-400 py-10 px-10 min-h-[90vh]">
+      <div className=" text-slate-400 font-bold md:text-[7vw] text-[10vw] px-10 flex bg-white justify-between">
+        <div>ABOUT</div>
+        <div>{`>>`}</div>
       </div>
-      <div className="w-full md:px-20 px-10 grid lg:grid-cols-2 grid-cols-1 items-center justify-around gap-7 ">
-        <Card Icon={<IoLogoGithub />} title={"Github"} content={"yunchan312"} />
-        <Card
-          Icon={<FaInstagram />}
-          title={"Instagram"}
-          content={"@9uccichann"}
-        />
+      <TextSlider />
+      <div className="w-full grid lg:grid-cols-2 grid-cols-1 items-center justify-around gap-7 text-slate-400">
+        <Card Icon={<IoLogoGithub />} title={"Github"} />
+        <Card Icon={<FaInstagram />} title={"Instagram"} />
         <Card Icon={<SiNotion />} title={"Notion"} />
-        <Card Icon={<FaPhone />} title={"Phone"} />
-
-        <Card
-          Icon={<MdOutlineEmail />}
-          title={"Email"}
-          content={"phenomena312@naver.com"}
-        />
-        <Card
-          Icon={<FaSchool />}
-          title={"University"}
-          content={"Hankuk Universiy of Foreign Studies"}
-        />
+        <Card Icon={<MdOutlineEmail />} title={"Email"} />
+        <Card Icon={<FaSchool />} title={"University"} />
       </div>
     </div>
   );
