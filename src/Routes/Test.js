e3fa6a932478188b1a9c5be8c2e01df38603e92e
yunchan312 <phenomenal312@hufs.ac.kr>
2024-02-12 23:59:11 +0900

    scroll animation test

--- src/Routes/Test.js
+++ src/Routes/Test.js
@@ -4,6 +4,17 @@ import { useRef } from "react";
 import mango from "../Assets/mango.jpg";
 
 export default function Test() {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["0%", "60%"],
+  });
+  const scale = useSpring(scrollYProgress, {
+    scale: 2,
+  });
+  const changeScaleX = useSpring(scrollYProgress, {
+    scaleX: 0.2,
+  });
   return (
     <>
       <div className="bg-orange-500 h-[100vh] flex flex-col justify-center items-center w-full border-2">
@@ -36,16 +47,35 @@ export default function Test() {
           </motion.div>
         }
       </div>
-      <div className="bg-blue-500 h-[90vh] flex flex-col justify-center items-center w-full border-2">
-        <motion.div
-          ref={ref}
-          style={{ scaleX }}
-          className="bg-white w-[30vw] h-[30vw] rounded-sm border-2"
-        />
+      <div className="bg-blue-500 h-[100vh] flex flex-col justify-center items-center w-full border-2">
+        {
+          <motion.circle
+            cx={500}
+            animate={{ cx: [null, 100, 200] }}
+            transition={{ duration: 3, times: [0, 0.2, 1] }}
+            className="bg-white w-[30vw] h-[30vh] rounded-sm border-2"
+          ></motion.circle>
+        }
       </div>
 
       <div className="bg-yellow-500 h-[100vh] pr-10">
         <Btn name={"PROFILE"} />
+      </div>
+      <div className="bg-green-500 h-[100vh] pr-10 flex justify-between items-center relative overflow-hidden">
+        <div className="w-[30vw] h-full bg-orange-500 relative z-20 font-bold text-white p-10 text-9xl">
+          MANGO
+        </div>
+        <div className="bg-orange-500 w-full h-[100vh] pr-10 flex justify-center items-center overflow-hidden absolute">
+          <motion.img
+            style={{ scale }}
+            src={mango}
+            className="w-[50vw] bg-white border-2 border-red-500 absolute z-10"
+          ></motion.img>
+        </div>
+        <div className="w-[30vw] h-full bg-orange-500 relative z-20"></div>
+      </div>
+      <div className="bg-slate-500 h-[100vh] pr-10 flex justify-center items-center ">
+        <motion.div className="w-[100px] h-[100px] bg-white"></motion.div>
       </div>
     </>
   );
