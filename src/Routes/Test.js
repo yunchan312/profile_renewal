b06f16c7e0af2d4bf8e835682e741a4460200697
yunchan123 <phenomenal312@hufs.ac.kr>
2024-02-12 16:37:18 +0900

    animation test

--- src/Routes/Test.js
+++ src/Routes/Test.js
@@ -1,7 +1,20 @@
-import { motion } from "framer-motion";
+import { motion, useScroll, useSpring } from "framer-motion";
 import Btn from "../Components/Btn";
+import { useRef } from "react";
+import mango from "../Assets/mango.jpg";
 
 export default function Test() {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end center"],
+  });
+  const scale = useSpring(scrollYProgress, {
+    scale: 2,
+  });
+  const changeScaleX = useSpring(scrollYProgress, {
+    scaleX: 0.2,
+  });
   return (
     <>
       <div className="bg-orange-500 h-[100vh] flex flex-col justify-center items-center w-full border-2">
@@ -47,6 +60,27 @@ export default function Test() {
 
       <div className="bg-yellow-500 h-[100vh] pr-10">
         <Btn name={"PROFILE"} />
+      </div>
+      <div className="bg-green-500 h-[100vh] pr-10 flex justify-around items-center ">
+        {/*<motion.div
+          initial={{ scale: 1 }}
+          style={{ changeScaleX }}
+          className="bg-black w-[30vw] h-full z-20 relative"
+        ></motion.div> */}
+        {
+          <motion.img
+            src={mango}
+            className="w-[50vw] bg-white border-2 border-red-500 absolute z-10"
+          ></motion.img>
+        }
+        {/*<motion.div
+          initial={{ scale: 1 }}
+          style={{ changeScaleX }}
+          className="bg-black w-[30vw] h-full z-20 relative"
+        ></motion.div> */}
+      </div>
+      <div className="bg-slate-500 h-[100vh] pr-10 flex justify-center items-center ">
+        <motion.div className="w-[100px] h-[100px] bg-white"></motion.div>
       </div>
     </>
   );
