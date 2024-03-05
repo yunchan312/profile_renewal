017de6129c5330226c80e3685e708163fc14ac0c
yunchan312 <phenomenal312@hufs.ac.kr>
2024-03-05 22:52:11 +0900

    fix: scrollx

--- src/Routes/Test/scrollx.js
+++ src/Routes/Test/scrollx.js
@@ -43,8 +43,8 @@ export default function ScrollSize() {
       },
       height: "500px",
       opacity: 1,
-      duration: 1.5,
-      delay: 1.5,
+      duration: 1,
+      delay: 1.3,
     });
     gsap.to("#text2", {
       scrollTrigger: {
@@ -54,8 +54,8 @@ export default function ScrollSize() {
 
       height: "500px",
       opacity: 1,
-      duration: 1.5,
-      delay: 1.5,
+      duration: 1,
+      delay: 1.3,
     });
     gsap.to("#text3", {
       scrollTrigger: {
@@ -65,31 +65,29 @@ export default function ScrollSize() {
 
       height: "500px",
       opacity: 1,
-      duration: 1.5,
-      delay: 1.5,
+      duration: 1,
+      delay: 1.3,
     });
   }, []);
   return (
     <div className="p-20 w-full gap-48 flex flex-col">
       <div className="flex gap-10 items-center overflow-hidden">
         <div
-          id="p1"
-          className={`relative z-10 h-[500px] w-[0px] bg-coding bg-cover bg-right`}
+          id="p3"
+          className={`relative z-10 h-[500px] w-[0px] bg-night bg-cover`}
         />
         <div
-          id="text"
+          id="text3"
           className=" w-[50%] px-10 h-[0px] opacity-0  flex flex-col justify-center py-10"
         >
-          <div className="text-5xl font-bold">DEVELOPER</div>
+          <div className="text-5xl font-bold">TRAVELER</div>
           <div className="text-lg">
-            개발을 좋아하고 즐기는 <strong>'개발자'</strong>입니다.
-            <br />
-            개인 개발 및 협업을 통해 만들고자 하는 서비스나 프로그램을
-            개발합니다.
+            여행을 통해 일상에서 벗어나 휴식하는 것 뿐만 아니라
+            <br /> 다양한 경험을 쌓고 식견을 넓히는 <strong>'여행가'</strong>
+            입니다.
           </div>
         </div>
       </div>
-
       <div className="flex gap-10 items-center overflow-hidden">
         <div
           id="text2"
@@ -109,18 +107,19 @@ export default function ScrollSize() {
 
       <div className="flex gap-10 items-center overflow-hidden">
         <div
-          id="p3"
-          className={`relative z-10 h-[500px] w-[0px] bg-night bg-cover`}
+          id="p1"
+          className={`relative z-10 h-[500px] w-[0px] bg-coding bg-cover bg-right`}
         />
         <div
-          id="text3"
+          id="text"
           className=" w-[50%] px-10 h-[0px] opacity-0  flex flex-col justify-center py-10"
         >
-          <div className="text-5xl font-bold">TRAVELER</div>
+          <div className="text-5xl font-bold">DEVELOPER</div>
           <div className="text-lg">
-            여행을 통해 일상에서 벗어나 휴식하는 것 뿐만 아니라
-            <br /> 다양한 경험을 쌓고 식견을 넓히는 <strong>'여행가'</strong>
-            입니다.
+            개발을 좋아하고 즐기는 <strong>'개발자'</strong>입니다.
+            <br />
+            개인 개발 및 협업을 통해 만들고자 하는 서비스나 프로그램을
+            개발합니다.
           </div>
         </div>
       </div>
