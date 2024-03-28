ad8ee5fd97e1c7813b344026cc3f41b098cd46aa
yunchan123 <phenomenal312@hufs.ac.kr>
2024-03-28 18:23:23 +0900

    final fix

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import Home from "./Routes/Home";
 import { Route, Routes } from "react-router-dom";
-import ProflioDetail from "./Pages/proflieDetails/ProfileDetail";
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/project/profile" element={<ProflioDetail />} />
       </Routes>
     </div>
   );
--- src/Components/Btn.js
+++ src/Components/Btn.js
@@ -1,18 +1,10 @@
-import { useNavigate } from "react-router-dom";
-export default function Btn({ title, content, state, nav }) {
-  const navigate = useNavigate();
-  const handleOnClick = () => {
-    navigate(`/project/${nav}`);
-  };
+export default function Btn({ title, content, state }) {
   return (
     <>
       <div className="w-[40%]">
         <div className="text-3xl font-bold">{title}</div>
         <div className="">{content}</div>
-        <span
-          className="text-2xl font-bold text-gray-300 hover:cursor-pointer hover:shadow-lg py-2 pr-8"
-          onClick={handleOnClick}
-        >
+        <span className="text-2xl font-bold text-gray-300 hover:cursor-pointer hover:shadow-lg py-2 pr-8">
           {state ? "작업중입니다." : "See Details"}
         </span>
       </div>
